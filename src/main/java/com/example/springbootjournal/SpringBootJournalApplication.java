583fca69d764a63ae144a4fbb280f4e3ab668542
Omkar Rane <omkarrane10@gmail.com>
2020-04-23 03:27:02 +0530

    Added Data REST dependency and MySQL configuration

--- src/main/java/com/example/springbootjournal/SpringBootJournalApplication.java
+++ src/main/java/com/example/springbootjournal/SpringBootJournalApplication.java
@@ -12,16 +12,6 @@ import java.util.Date;
 @SpringBootApplication
 public class SpringBootJournalApplication {
 
-	@Bean
-	InitializingBean saveData(JournalRepository repo) {
-		return () -> {
-			repo.save(new Journal("Get to know Spring Boot", new Date("16/04/2020"), "Today I will learn Spring Boot"));
-			repo.save(new Journal("Simple Spring Boot Project", new Date("17/04/2020"), "I will do my first Spring Boot Project"));
-			repo.save(new Journal("Spring Boot Reading", new Date("17/04/2020"), "Read more about Spring Boot"));
-			repo.save(new Journal("Spring Boot in the Cloud", new Date("20/04/2020"), "Spring Boot using Cloud Foundry"));
-		};
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJournalApplication.class, args);
 	}
